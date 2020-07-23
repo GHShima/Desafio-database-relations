047fab9e018e6f51392bbaf14cd6fc3c901e283b
Shima <guilherme.shima91@hotmail.com>
2020-07-23 09:17:16 -0300

    finalização

--- jest.config.js
+++ jest.config.js
@@ -138,7 +138,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/src/__tests__/**/*.[jt]s?(x)'],
+  //testMatch: ['**/src/__tests__/**/*.[jt]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
